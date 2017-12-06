74d2fef73ee89ba8749e0caa1a1a5514db507cd2
haynal <haynal@haynal-PC>
2017-12-06 10:05:42 -0500

    Done ImageLoad!

---
+++ 4U.Assignment 6/src/ImageLoad.java
@@ -0,0 +1,30 @@
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author haynal
+ */
+public class ImageLoad {
+
+    public void ImageLoad() {
+        // load the images
+        BufferedImage pic = null;
+        try {
+            String file = null;
+            pic = ImageIO.read(new File("images/" + file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
