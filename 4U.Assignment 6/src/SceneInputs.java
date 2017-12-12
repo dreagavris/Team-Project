bb910eb37b15b2d1a1490e92594c77717c62664b
haynal <haynal@haynal-PC>
2017-12-12 11:16:45 -0500

    No more Scene, fixing SceneInputs, just need to figure out how to use ImageLoad in SceneInputs

--- 4U.Assignment 6/src/SceneInputs.java
+++ 4U.Assignment 6/src/SceneInputs.java
@@ -1,37 +1,91 @@
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URL;
+import java.util.Scanner;
+import javax.imageio.ImageIO;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author ratid6445
  */
 public class SceneInputs {
-    private char dir;
-    private Locations image;
+
+    private String dir;
+    private BufferedImage image;
     private boolean isBlocked;
-    private char nextDir;
-    private Scene nextLoc;
-    
-    public SceneInputs(char direction){
+    private String nextDir;
+    private String nextLoc;
+    FileReader textFile;
+
+    public SceneInputs() {
+        // read the text file
+        Scanner in = new Scanner(textFile);
+
+        // get the direction
+        dir = in.next();
+        
+        // get the image
+        String theImage = in.next();
+        // ImageLoader text file
+        ImageLoad.class.equals(theImage);
+        
+        // get the following information
+        // since the next information is a boolean, must make a string first to get it
+        String next = in.next();
+        // see if the next input is true or false
+        if (next.equals("false")) {
+            // if input is false then it is blocked
+            isBlocked = false;
+            //get the next inputs
+            // next input gives the next direction
+            nextDir = in.next();
+            // the next input is the next location
+            nextLoc = in.next();
+            // move to the next line
+            in.nextLine();
+            // if the next input is true
+        } else {
+            // then it is not blocked
+            isBlocked = true;
+            //move to the next line because no more inputs after true
+            in.nextLine();
+        }
+    }
+
+    // get the direction
+    public String getDirection(String direction) {
         this.dir = direction;
+        return direction;
     }
     
-    public void image(Locations image){
+    // get the image
+    public BufferedImage getImage(BufferedImage image) {
         this.image = image;
+        return image;
     }
     
-    public void isBlocked(boolean isBlocked){
+    // see if can move
+    public boolean isBlocked(boolean isBlocked) {
         this.isBlocked = isBlocked;
+        return isBlocked;
     }
-    
-    public void nextDirection(char nextDir){
+
+    // get the next direction
+    public String getNextDirection(String nextDir) {
         this.nextDir = nextDir;
+        return nextDir;
     }
-    
-    public void nextLoc(Scene nextLoc){
+
+    // get the next location
+    public String getNextLoc(String nextLoc) {
         this.nextLoc = nextLoc;
+        return nextLoc;
     }
 }
